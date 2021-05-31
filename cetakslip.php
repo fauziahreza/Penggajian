e24f09e5ce7ae79f32e41786863238b43474044b
Fanada <fajarn680@gmail.com>
2021-06-01 03:19:56 +0700

    fixing except layout

--- cetakslip.php
+++ cetakslip.php
@@ -11,104 +11,106 @@
         <h4>COMPANY X</h4>
     </center>
 
-    <tbody>
-        <tr>
-            <th scope="row">
-                Company Name
-            </th>
-            <td>
-                Perusahaan X
-            </td>
-            <td>
+    <table class="table align-items-center table-flush">
 
-            </td>
-            <th scope="row">
-                ID Employe
-            </th>
-            <td>
-                1
-            </td>
-        </tr>
-        <tr>
-            <th scope="row">
-                Created
-            </th>
-            <td>
-                Admin
-            </td>
-            <td>
-
-            </td>
-            <th scope="row">
-                Staff Name
-            </th>
-            <td>
-                Fauzia Reza
-            </td>
-        </tr>
-        <tr>
-            <th scope="row">
-                Period
-            </th>
-            <td>
-                04-2021
-            </td>
-            <td>
-
-            </td>
-            <th scope="row">
-                Position
-            </th>
-            <td>
-                Mobile Developer
-            </td>
-        </tr>
-        <tr>
-            <th scope="row">
-
-            </th>
-            <td>
-
-            </td>
-            <td>
-
-            </td>
-            <th scope="row">
-
-            </th>
-            <td>
-
-            </td>
-        </tr>
-        <tr>
-            <th scope="row">
-                Basic Salary
-            </th>
-            <td>
-                Rp 5.000.000
-            </td>
-
-        </tr>
-        <tr>
-            <th scope="row">
-                Overtime Pay
-            </th>
-            <td>
-                Rp 0
-            </td>
-
-        </tr>
-        <tr>
-            <th scope="row">
-                Total Received
-            </th>
-            <td>
-                <strong> Rp 5.000.000</strong>
-            </td>
-        </tr>
-    </tbody>
-    </table>
-    </div>
+                <tbody>
+                    <tr>
+                        <th scope="row">
+                            Company Name
+                        </th>
+                        <td>
+                            Perusahaan X
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <th scope="row">
+                            ID Employe
+                        </th>
+                        <td>
+                            1
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row">
+                            Created
+                        </th>
+                        <td>
+                            Admin
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <th scope="row">
+                            Staff Name
+                        </th>
+                        <td>
+                            Fauzia Reza
+                        </td>
+                    </tr>
+					<tr>
+                        <th scope="row">
+                            Period
+                        </th>
+                        <td>
+                            04-2021
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <th scope="row">
+                            Position
+                        </th>
+                        <td>
+                            Mobile Developer
+                        </td>
+                    </tr>
+					<tr>
+                        <th scope="row">
+                            
+                        </th>
+                        <td>
+                            
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <th scope="row">
+                            
+                        </th>
+                        <td>
+                            
+                        </td>
+                    </tr>
+					<tr>
+                        <th scope="row">
+                            Basic Salary
+                        </th>
+                        <td>
+                            Rp 5.000.000
+                        </td>
+                        
+                    </tr>
+					<tr>
+                        <th scope="row">
+                            Overtime Pay
+                        </th>
+                        <td>
+                            Rp 0
+                        </td>
+                        
+                    </tr>
+					<tr>
+                        <th scope="row">
+                            Total Received
+                        </th>
+                        <td>
+                            <strong> Rp 5.000.000</strong>
+                        </td>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
     <script>
         window.print();
     </script>
